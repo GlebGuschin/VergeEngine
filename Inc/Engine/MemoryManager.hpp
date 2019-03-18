93ead532f33a8e8a99e36e0890241aec19673a45
Gleb Guschin <hotdraw@mail.ru>
2019-03-18 13:56:26 +0300

    init MemoryManager

---
+++ Inc/Engine/MemoryManager.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "DynamicArray.hpp"
+
+namespace Verge3D {
+
+	struct MemoryBufferInfo {
+	size_t size;
+	unsigned flags;
+};
+
+
+class MemoryBuffer : public Referenceable {
+//class MemoryBuffer {
+
+	size_t size;
+	void* ptr;
+	MemoryManager* manager;
+
+protected:
+
+	unsigned lockCounter; // atomic ?
+
+public:
+
+	MemoryBuffer() : size(0), lockCounter(0), ptr(nullptr) {}
+
+	MemoryBuffer(MemoryManager* manager_, void* ptr_, size_t size_, unsigned flags=0) : 
+		manager(manager_),
+		size(size_), 
+		ptr(ptr_), 
+		lockCounter(0) {
+	
+	}
+
+	MemoryBuffer(void* ptr_, size_t size_) : size(size_), ptr(ptr_), lockCounter(0) {}
+	~MemoryBuffer();
+	/*
+	~MemoryBuffer() {	
+		lockCounter = 0;
+	}
+	*/
+
+
+	size_t getSize() const { return size; }
+	void* getPtr() const { return ptr; }
+
+	virtual void* lock(unsigned flags = 0) {
+		lockCounter++;
+		return ptr;
+	}
+
+	virtual void  unlock() {
+		lockCounter--;
+	}
+
+};
+
+
+
+class MemoryManager {
+
+	DynamicArray<MemoryBuffer*> buffers;
+
+public:
+
+	MemoryBuffer* createMemoryBuffer(size_t size, unsigned flags = 0); 
+	MemoryBuffer* createMemoryBuffer(MemoryBuffer* parentBuffer, size_t size, size_t offset, unsigned flags = 0);
+	void destroyMemoryBuffer(MemoryBuffer*);
+
+};
+
+
+};
